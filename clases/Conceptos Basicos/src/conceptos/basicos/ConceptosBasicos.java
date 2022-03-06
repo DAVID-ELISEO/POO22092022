928bb721118fab22e03f89a87b64b80b97722b5d
DAVID-ELISEO <davideliseo90@aragon.unam.mx>
2022-03-05 23:23:37 -0600

    Sesion 1 marzo, Automovil

--- clases/Conceptos Basicos/src/conceptos/basicos/ConceptosBasicos.java
+++ clases/Conceptos Basicos/src/conceptos/basicos/ConceptosBasicos.java
@@ -4,6 +4,10 @@
  */
 package conceptos.basicos;
 
+import ico.fes.Automovil;
+import ico.fes.Silla;
+import java.awt.Color;
+
 /**
  *
  * @author davideliseo
@@ -16,7 +20,24 @@ public class ConceptosBasicos {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola de nuevo"); 
-                 
+        Silla s1=new Silla();
+        System.out.println("s1");//por defecto se invoca tostring()
+        System.out.println("s1,tostring");
+        
+        
+        Automovil miBocho=new Automovil();
+        System.out.println("miBocho");
+        miBocho.setMarca("VW");
+        miBocho.setMarca("Sedan");
+        miBocho.setModelo(1970);
+        miBocho.setColor(Color.BLUE);
+        System.out.println("miBocho");
+        
+        Automovil miMustang=new Automovil("ford", "Mustang", 2010, Color.yellow);
+        System.out.println("miMustang");
+        
+        Automovil miAkura=new Automovil("Akura", "NSX", 2013, Color.gray);
+        
     }
     
 }
